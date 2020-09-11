9fc2e30b05258f76178b6228ab85dbc6cba10a1d
Ari Stathopoulos <aristath@gmail.com>
2020-09-11 10:21:32 +0300

    More WIP for the basics

--- block-template-parts/header.php
+++
@@ -1,9 +0,0 @@
-<!-- wp:group -->
-<div class="wp-block-group"><div class="wp-block-group__inner-container"><!-- wp:site-logo /--></div></div>
-<!-- /wp:group -->
-
-<!-- wp:navigation {"orientation":"horizontal"} -->
-<!-- wp:navigation-link {"label":"Home","url":"/"} /-->
-
-<!-- wp:navigation-link {"label":"Blog","url":"/blog"} /-->
-<!-- /wp:navigation -->
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-// Add wp_head() and wp_footer to avoid breaking things and at least show the adminbar.
-wp_head();
-wp_footer();
+get_header();
+
+if ( current_user_can( 'activate_plugins' ) ) {
+    echo '<div style="max-width:40em;margin:3em auto;padding:4em;background:#000;color:#fff;">';
+    _e( 'This theme requires the Gutenberg plugin installed. Please visit your dashboard and follow the instructions on the notification displayed to enable the plugin and the Full Site Editing experiment.' );
+    echo '</div>';
+}
+get_footer();
