6bb7b5caabad3c806f71d99ebe5d512e8627ba14
Ari Stathopoulos <aristath@gmail.com>
2020-11-18 10:55:29 +0200

    CS

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ add_action(
 		add_theme_support( 'widgets-block-editor' );
 		add_theme_support( 'block-nav-menus' );
 
-		// Experimental - See https://github.com/WordPress/gutenberg/pull/25220
+		// Experimental - See https://github.com/WordPress/gutenberg/pull/25220.
 		add_theme_support( 'split-block-styles' );
 
 		// Support a custom color palette.
