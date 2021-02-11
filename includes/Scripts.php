9e0bcd890404d05adf10b1d979ca14430bd782c1
Ari Stathopoulos <aristath@gmail.com>
2021-02-11 10:41:21 +0200

    Split skip-link script to separate file.

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -32,60 +32,9 @@ class Scripts {
 	 * @return void
 	 */
 	public function skip_link() {
-		?>
-		<script>
-			function injectSkipLink() {
-				var searchEl = [
-						'.wp-block-post-title',
-						'.wp-block-query-loop',
-						'.wp-block-post-content',
-						'.entry-content',
-						'h1',
-						'h2',
-					],
-					contentEl, 
-					contentElID, 
-					skipLink,
-					parentEl,
-					i;
-
-				// Find the content element.
-				for ( i = 0; i < searchEl.length; i++ ) {
-					if ( ! contentEl ) {
-						contentEl = document.querySelector( searchEl[ i ] );
-					}
-				}
-
-				// Early exit if no content element was found.
-				if ( ! contentEl ) {
-					return;
-				}
-
-				// Get the ID of the content element.
-				contentElID = contentEl.id;
-				if ( ! contentElID ) {
-					contentElID = 'auto-skip-link-target';
-					contentEl.id = contentElID;
-				}
-
-				// Get the parent element. This is where we'll inject the skip-link.
-				parentEl = document.querySelector( '.wp-site-blocks' );
-				if ( ! parentEl ) {
-					parentEl = document.body;
-				}
-
-				// Create the skip link.
-				skipLink = document.createElement( 'a' );
-				skipLink.classList.add( 'skip-link' );
-				skipLink.classList.add( 'screen-reader-text' );
-				skipLink.href = '#' + contentElID;
-				skipLink.innerHTML = '<?php esc_attr_e( 'Skip to content', 'q' ); ?>';
-
-				// Inject the skip link.
-				parentEl.insertAdjacentElement( 'afterbegin', skipLink );
-			}
-			injectSkipLink();
-		</script>
-		<?php
+		echo '<script>';
+		echo 'window.skipToContent="' . esc_html__( 'Skip to content', 'q' ) . '";';
+		include get_theme_file_path( 'scripts/skip-link.js' );
+		echo '</script>';
 	}
 }
