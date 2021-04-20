9a1c04c1fba6f019afd71411a92987836adce537
Ari Stathopoulos <aristath@gmail.com>
2021-04-20 14:56:07 +0300

    Are you happy now PHPStan?

--- functions.php
+++ functions.php
@@ -53,6 +53,12 @@ add_filter(
 	}
 );
 
+if ( ! function_exists( 'gutenberg_get_block_template' ) ) {
+	function gutenberg_get_block_template() {
+		return false;
+	}
+}
+
 if ( ! function_exists( 'gutenberg_do_block_template_part' ) ) {
 	/**
 	 * Print a template-part.
@@ -64,14 +70,12 @@ if ( ! function_exists( 'gutenberg_do_block_template_part' ) ) {
 	 * @return void
 	 */
 	function gutenberg_do_block_template_part( $part ) {
-		if ( function_exists( 'gutenberg_get_block_template' ) ) {
-			$template_part = gutenberg_get_block_template( get_stylesheet() . '//' . $part, 'wp_template_part' );
-			if ( ! $template_part || empty( $template_part->content ) ) {
-				return;
-			}
-
-			echo do_blocks( $template_part->content ); // phpcs:ignore WordPress.Security.EscapeOutput
+		$template_part = gutenberg_get_block_template( get_stylesheet() . '//' . $part, 'wp_template_part' );
+		if ( ! $template_part || empty( $template_part->content ) ) {
+			return;
 		}
+
+		echo do_blocks( $template_part->content ); // phpcs:ignore WordPress.Security.EscapeOutput
 	}
 }
 
