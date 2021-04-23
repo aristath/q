3bd01a176b600f9a7700f464d400b43594f9bfcc
Ari Stathopoulos <aristath@gmail.com>
2021-04-23 13:06:07 +0300

    remove things that are now in core

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -21,22 +21,7 @@ class Scripts {
 	 * @since 1.0
 	 */
 	public function __construct() {
-		add_action( 'wp_footer', [ $this, 'skip_link' ] );
 		add_action( 'wp_head', [ $this, 'remove_embed_script' ] );
-	}
-
-	/**
-	 * Print a skip link.
-	 *
-	 * @access public
-	 * @since 1.0
-	 * @return void
-	 */
-	public function skip_link() {
-		echo '<script>';
-		echo 'window.skipToContent="' . esc_html__( 'Skip to content', 'q' ) . '";';
-		include get_theme_file_path( 'scripts/skip-link.js' );
-		echo '</script>';
 	}
 
 	/**
