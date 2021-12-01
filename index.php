4abce1986d3a403e28e48de62527bdb9b08e161c
Ari Stathopoulos <aristath@gmail.com>
2021-12-01 13:10:53 +0200

    index.php file doesn't need any content anymore

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Fallback file in case Gutenberg and Full Site Editing are not enabled.
- *
- * @package aristath/q
- *
- * @since 1.0
- */
-
-get_header();
-
-if ( current_user_can( 'activate_plugins' ) ) {
-	echo '<div style="max-width:40em;margin:3em auto;padding:4em;background:#000;color:#fff;">';
-	esc_html_e( 'This theme requires the Gutenberg plugin installed. Please visit your dashboard and follow the instructions on the notification displayed to enable the plugin and the Full Site Editing experiment.', 'q' );
-	echo '</div>';
-}
-get_footer();
+// There is nothing output here because block themes do not use php templates.
+// There is a core ticket discussing removing this requirement for block themes:
+// https://core.trac.wordpress.org/ticket/54272.
